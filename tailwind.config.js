361b0f54f1cdb34bfdb27c69c71fa9a0efc9c43b
Krishna Gupta <137400670+Krishnagupta2004@users.noreply.github.com>
2024-09-11 00:37:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      boxShadow: {
+        custom:
+          "0px 3px 6px rgba(0, 0, 0, 0.16), 0px 3px 6px rgba(0, 0, 0, 0.23)",
+      },
+    },
+  },
+};
